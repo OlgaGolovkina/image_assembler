cbff9a2538f1dae15b0dd246ca1ed64ecb9c3b6b
Olga Golovkina <ogolovkina13@gmail.com>
2020-01-15 01:16:05 +0100

    Evolution solution for Genetic Algorithm was created. Crossover mechanism, selection method were added

---
+++ image_solver/genetic_algorithm.py
@@ -0,0 +1,69 @@
+from operator import attrgetter
+from . import image_helpers
+from .selection import roulette_selection
+from .crossover import Crossover
+from .individual import Individual
+from .image_analysis import ImageAnalysis
+from .progress_bar import print_progress
+
+
+class GeneticAlgorithm:
+
+    TERMINATION_THRESHOLD = 10
+
+    def __init__(self, image, piece_size, population_size, generations, elite_size=2):
+        self._image = image
+        self._piece_size = piece_size
+        self._generations = generations
+        self._elite_size = elite_size
+        pieces, rows, columns = image_helpers.flatten_image(image, piece_size, indexed=True)
+        self._population = [Individual(pieces, rows, columns) for _ in range(population_size)]
+        self._pieces = pieces
+
+    def start_evolution(self):
+        print(f"=== Pieces:      {len(self._pieces)}\n")
+
+        ImageAnalysis.analyze_image(self._pieces)
+
+        fittest = None
+        best_fitness_score = float("-inf")
+        termination_counter = 0
+
+        for generation in range(self._generations):
+            print_progress(generation, self._generations - 1, prefix="=== Solving image: ")
+
+            new_population = []
+
+            elite = self._get_elite_individuals(elites=self._elite_size)
+            new_population.extend(elite)
+
+            selected_parents = roulette_selection(self._population, elites=self._elite_size)
+
+            for first_parent, second_parent in selected_parents:
+                crossover = Crossover(first_parent, second_parent)
+                crossover.run()
+                child = crossover.child()
+                new_population.append(child)
+
+            fittest = self._best_individual()
+
+            if fittest.fitness <= best_fitness_score:
+                termination_counter += 1
+            else:
+                best_fitness_score = fittest.fitness
+
+            if termination_counter == self.TERMINATION_THRESHOLD:
+                print("\n\n=== GA terminated")
+                print("=== There was no improvement for {} generations".format(self.TERMINATION_THRESHOLD))
+                return fittest
+
+            self._population = new_population
+        return fittest
+
+    def _get_elite_individuals(self, elites):
+        """Returns first 'elite_count' fittest individuals from population"""
+        return sorted(self._population, key=attrgetter("fitness"))[-elites:]
+
+    def _best_individual(self):
+        """Returns the fittest individual from population"""
+        return max(self._population, key=attrgetter("fitness"))
---
+++ image_solver/selection.py
@@ -0,0 +1,29 @@
+"""Selects fittest individuals from given population."""
+import random
+import bisect
+
+
+def roulette_selection(population, elites=4):
+    """Roulette wheel selection.
+
+    Each individual is selected to reproduce, with probability directly
+    proportional to its fitness score.
+
+    :params population: Collection of the individuals for selecting.
+    :params elite: Number of elite individuals passed to next generation.
+    """
+    fitness_values = [individual.fitness for individual in population]
+    probability_intervals = [sum(fitness_values[:i + 1]) for i in range(len(fitness_values))]
+
+    def select_individual():
+        """Selects random individual from population based on fittest value"""
+        random_select = random.uniform(0, probability_intervals[-1])
+        selected_index = bisect.bisect_left(probability_intervals, random_select)
+        return population[selected_index]
+
+    selected = []
+    for i in range(len(population) - elites):
+        first, second = select_individual(), select_individual()
+        selected.append((first, second))
+
+    return selected
